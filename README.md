5bc08bb8853c9203bdf4824aae66382b59c229d8
= <=>
2024-03-29 14:33:43 +0300

    no chalk

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ### Usage::
-- Download via `npm i inite -g`
+- Download via `npm i inite -g` or clone repository, open it in terminal and run `npm i -g`
 - Open folder with your future project and rum in terminal `inite`. CLI will automatically initialize git, npm, create .gitignore, index.js and .env
 
 Feel free to contribute or open isues <3
--- bin/index.js
+++ bin/index.js
@@ -1,15 +1,11 @@
 #!/usr/bin/env node
 
-import readline from 'readline';
 import Initializator from './Initializator.js';
-import chalk from 'chalk';
 
 const init = new Initializator();
-const prompts = readline.createInterface(process.stdin, process.stdout);
 
-console.log(chalk.green('Initialization...'))
+console.log("\x1b[42m Initializing... \x1b[0m");
 
 init.init(() => {
-    prompts.close();
-    console.log(chalk.green('Done!'));
+    console.log("\x1b[42m Done! \x1b[0m");
 })
